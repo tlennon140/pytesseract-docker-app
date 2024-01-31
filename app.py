825ca21fd94e7afbb915cbceba668263571f3f16
Terry Lennon <43939096+tlennon140@users.noreply.github.com>
2024-01-31 13:07:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
 from PIL import Image
 import pytesseract
+from minio import Minio
 
 app = Flask(__name__)
 
 @app.route('/')
 def ocr():
     # test.png from the pytesseract project: https://github.com/madmaze/pytesseract/tree/master/tests/data
+    
+    client = Minio("minio-server-lrgd.onrender.com",
+        access_key="vG+Csz1r01GQJPlHR6rybVB/1UpJ6iN/UEI75X48Kgw=",
+        secret_key="bdklcAgSv2W9OLic7eelfdWcu/tQjuONKoWtp47P35s=",
+    )
+
+    bucket_name = "hact-reports"
+    #bucket_name = "prodocs"
+
+    objects = client.list_objects(bucket_name)
+    for obj in objects:
+        print(obj)
+
     return pytesseract.image_to_string(Image.open('test.png'))
